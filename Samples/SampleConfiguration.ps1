e8df3c38919d1c58679419532b84938457f6ece4
Travis Plunk <travis.plunk@microsoft.com>
2015-04-24 09:29:13 -0700

    Add sample to use to test End to End

--- Samples/SampleConfiguration.ps1
+++ Samples/SampleConfiguration.ps1
@@ -1,6 +1,11 @@
 ﻿configuration Sample_xDhcpsServerScope_NewScope 
  { 
      Import-DscResource -module xDHCpServer 
+     WindowsFeature DHCP
+     {
+        Name = 'DHCP'
+        Ensure = 'Present'
+     }
      xDhcpServerScope Scope 
      { 
          Ensure = 'Present' 
@@ -10,24 +15,27 @@
          SubnetMask = '255.255.255.0' 
          LeaseDuration = '00:08:00' 
          State = 'Active' 
-         AddressFamily = 'IPv4' 
+         AddressFamily = 'IPv4'
+         DependsOn = @('[WindowsFeature]DHCP') 
      } 
      xDhcpServerReservation PullServerIP 
      { 
          Ensure = 'Present' 
          ScopeID = '192.168.1.0' 
          ClientMACAddress = '00155D8A54A1' 
-         IPAddress = '192.168.10.2' 
+         IPAddress = '192.168.1.2' 
          Name = 'DSCPullServer' 
          AddressFamily = 'IPv4' 
+         DependsOn = @('[WindowsFeature]DHCP') 
      }  
      xDhcpServerOption Option 
      { 
          Ensure = 'Present' 
-         ScopeID = '192.168.10.0' 
+         ScopeID = '192.168.1.0' 
          DnsDomain = 'contoso.com' 
-         DnsServerIPAddress = '192.168.10.22','192.168.10.1' 
+         DnsServerIPAddress = '192.168.1.22','192.168.1.1' 
          AddressFamily = 'IPv4' 
-         Router = '192.168.10.1'
+         Router = '192.168.1.1'
+         DependsOn = @('[WindowsFeature]DHCP') 
      } 
  } 
