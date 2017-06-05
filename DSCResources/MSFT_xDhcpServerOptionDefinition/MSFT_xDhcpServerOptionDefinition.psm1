39ec094af9c8156ae231c39e40e5ba91543f8133
marciorschneider <marciorschneider@gmail.com>
2017-06-05 19:40:55 -0300

    test

--- DSCResources/MSFT_xDhcpServerOptionDefinition/MSFT_xDhcpServerOptionDefinition.psm1
+++ DSCResources/MSFT_xDhcpServerOptionDefinition/MSFT_xDhcpServerOptionDefinition.psm1
@@ -50,14 +50,14 @@ function Set-TargetResource
         [Parameter()] [ValidateNotNullOrEmpty()]
         [UInt32]$OptionID,
 
-        [Parameter()] [ValidateSet('Byte,Word,DWord,DWordDWord,IPv4Address,String,BinaryData,EncapsulatedData')]
+        [Parameter()] [ValidateSet('Byte','Word','DWord','DWordDWord','IPv4Address','String','BinaryData','EncapsulatedData')]
         [String]$Type,
 
         [Parameter()]
         [string]$Description,
         
         [Parameter()]
-        [bool]$MultiValued,
+        [bool]$MultiValued = $false,
 
         [Parameter()] [ValidateSet('IPv4')]
         [String]$AddressFamily = 'IPv4',
@@ -66,8 +66,42 @@ function Set-TargetResource
         [String]$Ensure = 'Present'
     )
 
+        
+    #reading the dhcp option
+    $dhcpServerOptionDefinition = Get-DhcpServerv4OptionDefinition -OptionId $OptionID -ErrorAction SilentlyContinue
+
+    #testing for present
+    if ($Ensure -eq 'Present')
+    {
+        #testing if exists
+        if ($dhcpServerOptionDefinition)
+        {
+            #if option exists we need only to adjust the parameters
+            Write-Verbose "Modifying DHCP Option Definition $OptionID"
+            set-DhcpServerv4OptionDefinition -OptionId $OptionID -name $Name
+        }
+
+        else
+        {
+            #if option does not exist we need to add it
+            write-verbose "Adding DHCP Option Definition $OptionID"
+            if ($MultiValued)
+            {
+                Add-DhcpServerv4OptionDefinition -OptionId $OptionID -name $Name -Type $Type -Description $Description -MultiValued
+            }
+            else
+            {
+                Add-DhcpServerv4OptionDefinition -OptionId $OptionID -name $Name -Type $Type -Description $Description
+            }
+
+        }
+    }
+    
+
+
+
 }
-function test-TargetResource
+function Test-TargetResource
 {
     [CmdletBinding()]
     [OutputType([System.Boolean])]
@@ -75,7 +109,7 @@ function test-TargetResource
     (
         [parameter()] [ValidateNotNullOrEmpty()]
         [String]$Name,
-
+         
         [Parameter()] [ValidateNotNullOrEmpty()]
         [UInt32]$OptionID,
 
@@ -86,7 +120,7 @@ function test-TargetResource
         [string]$Description,
         
         [Parameter()]
-        [bool]$MultiValued,
+        [bool]$MultiValued = $false,
 
         [Parameter()] [ValidateSet('IPv4')]
         [String]$AddressFamily = 'IPv4',
@@ -97,17 +131,7 @@ function test-TargetResource
 #region Input Validation
 
     # Check for DhcpServer module/role
-    Assert-Module -moduleName DHCPServer
-
-
-    #geting the dhcp option definition
-    $dhcpServerOptionDefinition = Get-DhcpServerv4OptionDefinition -OptionId $OptionID -ErrorAction SilentlyContinue -ErrorVariable err
-
-    if($err)
-    {
-        $errorMsg = $($LocalizedData.InvalidScopeIdMessage) -f $ScopeID
-        New-TerminatingError -errorId ScopeIdNotFound -errorMessage $errorMsg -errorCategory InvalidOperation
-    }
+    #Assert-Module -moduleName DHCPServer
 
 
 
@@ -117,30 +141,29 @@ function test-TargetResource
 try
 {
     #geting the dhcp option definition
+    Write-Verbose "Getting DHCP ServerOptionDefinition $OptionID"
     $dhcpServerOptionDefinition = Get-DhcpServerv4OptionDefinition -OptionId $OptionID -ErrorAction SilentlyContinue -ErrorVariable err
     
-
     #testing for Present
     if ($Ensure -eq 'Present')
     {
-        if (!$dhcpServerOptionDefinition)
+        if ($dhcpServerOptionDefinition)
         {
-            #dhcp server option does not exists
-            $Result = $false
+            #dhcp server option # exists
+            $Result = $true
         }
     }
-    # If Options should be absent, return False or remove it
+    # If option # not exists return false.
     else
     {
-        
+        $Result = $false
     }
     
 
-
-
 }
-catch()
+catch
 {
 }
 
+$Result
 }
