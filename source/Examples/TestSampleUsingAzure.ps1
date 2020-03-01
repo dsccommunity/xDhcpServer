69056fd1b08917fbfd6586500fd38ebd222c8caa
Johan Ljunggren <johan.ljunggren@shamn.se>
2020-03-01 15:20:14 +0100

    Update sample script

--- source/Examples/TestSampleUsingAzure.ps1
+++ source/Examples/TestSampleUsingAzure.ps1
@@ -3,15 +3,24 @@
 # wait a while, RDP into the machine and manually verify that the DHCP Server
 # is configured as specified in .\samples\SampleConfiguration.ps1
 
-Write-Verbose -Message 'Publishing configuration ...' -Verbose
-Publish-AzureVMDscConfiguration -ConfigurationPath .\Samples\SampleConfiguration.ps1  -Verbose -force
-Write-Verbose -Message 'Choosing VM ...' -Verbose
-$vm = Get-AzureVM | Out-GridView -Title 'choose vm to test with' -OutputMode Single
-if ($vm)
+function Invoke-SampleConfiguration
 {
-    Write-Verbose -Message 'Setting Extension ...' -Verbose
-    Set-AzureVMDscExtension -ConfigurationArchive SampleConfiguration.ps1.zip -ConfigurationName Sample_xDhcpsServerScope_NewScope -VM $vm -Verbose
-    Write-Verbose -Message 'Updating Vm ...' -Verbose
-    $vm | Update-AzureVM
+    Write-Verbose -Message 'Publishing configuration ...' -Verbose
+
+    Publish-AzureVMDscConfiguration -ConfigurationPath $PSScriptRoot\SampleConfiguration.ps1  -Verbose -force
+
+    Write-Verbose -Message 'Choosing VM ...' -Verbose
+
+    $vm = Get-AzureVM | Out-GridView -Title 'choose vm to test with' -OutputMode Single
+    if ($vm)
+    {
+        Write-Verbose -Message 'Setting Extension ...' -Verbose
+
+        Set-AzureVMDscExtension -ConfigurationArchive SampleConfiguration.ps1.zip -ConfigurationName SampleConfiguration -VM $vm -Verbose
+
+        Write-Verbose -Message 'Updating Vm ...' -Verbose
+
+        $vm | Update-AzureVM
+    }
+    Write-Verbose -Message 'Done' -Verbose
 }
-Write-Verbose -Message 'Done' -Verbose
