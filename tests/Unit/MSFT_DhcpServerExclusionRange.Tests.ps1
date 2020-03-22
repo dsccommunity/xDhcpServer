af538bdf4976a6266433104fc473b3fa7de8dda8
Johan Ljunggren <johan.ljunggren@shamn.se>
2020-03-22 11:43:49 +0100

    Clean up unit tests

--- tests/Unit/MSFT_DhcpServerExclusionRange.Tests.ps1
+++ tests/Unit/MSFT_DhcpServerExclusionRange.Tests.ps1
@@ -74,8 +74,8 @@ try
         }
 
         Describe 'xDhcpServer\Get-TargetResource' {
-            Mock Get-DhcpServerv4ExclusionRange -MockWith $getFakeDhcpExclusionRange
-            Mock Assert-Module -ParameterFilter { $ModuleName -eq 'DHCPServer' } { }
+            Mock -CommandName Get-DhcpServerv4ExclusionRange -MockWith $getFakeDhcpExclusionRange
+            Mock -CommandName Assert-Module
 
             It 'Should call "Assert-Module" to ensure "DHCPServer" module is available' {
                 $result = Get-TargetResource @testParams
@@ -85,83 +85,83 @@ try
 
             It 'Returns a "System.Collections.Hashtable" object type' {
                 $result = Get-TargetResource @testParams
-                $result | Should BeOfType [System.Collections.Hashtable]
+                $result | Should -BeOfType [System.Collections.Hashtable]
             }
 
             It 'Returns all correct values' {
-                Mock Get-DhcpServerv4ExclusionRange -MockWith $getFakeDhcpExclusionRange
+                Mock -CommandName Get-DhcpServerv4ExclusionRange -MockWith $getFakeDhcpExclusionRange
 
                 $result = Get-TargetResource @testParams
-                $result.Ensure | Should Be $ensure
-                $result.ScopeId | Should Be $scopeId
-                $result.IPStartRange | Should Be $ipStartRange
-                $result.IPEndRange | Should Be $ipEndRange
-                $result.AddressFamily | Should Be $addressFamily
+                $result.Ensure | Should -Be $ensure
+                $result.ScopeId | Should -Be $scopeId
+                $result.IPStartRange | Should -Be $ipStartRange
+                $result.IPEndRange | Should -Be $ipEndRange
+                $result.AddressFamily | Should -Be $addressFamily
             }
 
             It 'Returns the properties as $null when the exclusion does not exist' {
-                Mock Get-DhcpServerv4ExclusionRange { return $null }
+                Mock -CommandName Get-DhcpServerv4ExclusionRange { return $null }
 
                 $result = Get-TargetResource @testParams
-                $result.Ensure | Should Be 'Absent'
-                $result.ScopeId | Should Be $scopeId
-                $result.IPStartRange | Should Be $null
-                $result.IPEndRange | Should Be $null
-                $result.AddressFamily | Should Be $addressFamily
+                $result.Ensure | Should -Be 'Absent'
+                $result.ScopeId | Should -Be $scopeId
+                $result.IPStartRange | Should -Be $null
+                $result.IPEndRange | Should -Be $null
+                $result.AddressFamily | Should -Be $addressFamily
             }
         }
 
 
         Describe 'xDhcpServer\Test-TargetResource' {
-            Mock Assert-Module -ParameterFilter { $ModuleName -eq 'DHCPServer' } { }
+            Mock -CommandName Assert-Module
 
             It 'Returns a "System.Boolean" object type' {
-                Mock Get-DhcpServerv4ExclusionRange -MockWith $getFakeDhcpExclusionRange
+                Mock -CommandName Get-DhcpServerv4ExclusionRange -MockWith $getFakeDhcpExclusionRange
 
                 $result = Test-TargetResource @testParams -Ensure 'Present'
-                $result | Should BeOfType [System.Boolean]
+                $result | Should -BeOfType [System.Boolean]
             }
 
             It 'Returns $true when the exclusion exists and Ensure = Present' {
-                Mock Get-DhcpServerv4ExclusionRange -MockWith $getFakeDhcpExclusionRange
+                Mock -CommandName Get-DhcpServerv4ExclusionRange -MockWith $getFakeDhcpExclusionRange
 
                 $result = Test-TargetResource @testParams -Ensure 'Present'
-                $result | Should Be $true
+                $result | Should -Be $true
             }
 
             It 'Returns $false when the exclusion does not exist and Ensure = Present' {
-                Mock Get-DhcpServerv4ExclusionRange { return $null }
+                Mock -CommandName Get-DhcpServerv4ExclusionRange { return $null }
 
                 $result = Test-TargetResource @testParams -Ensure 'Present'
-                $result | Should Be $false
+                $result | Should -Be $false
             }
 
             It 'Returns $false when the exclusion exists and Ensure = Absent ' {
-                Mock Get-DhcpServerv4ExclusionRange -MockWith $getFakeDhcpExclusionRange
+                Mock -CommandName Get-DhcpServerv4ExclusionRange -MockWith $getFakeDhcpExclusionRange
 
                 $result = Test-TargetResource @testParams -Ensure 'Absent'
-                $result | Should Be $false
+                $result | Should -Be $false
             }
 
             It 'Throws RangeNotCorrect exception when the start range is greater than the end range' {
-                { Test-TargetResource @badRangeParams } | Should Throw "StartRange must be less than EndRange"
+                { Test-TargetResource @badRangeParams } | Should -Throw "StartRange must be less than EndRange"
             }
         }
 
         Describe 'xDhcpServer\Set-TargetResource' {
-            Mock Assert-Module -ParameterFilter { $ModuleName -eq 'DHCPServer' } { }
-            Mock Add-DhcpServerv4ExclusionRange
-            Mock Remove-DhcpServerv4ExclusionRange
+            Mock -CommandName Assert-Module
+            Mock -CommandName Add-DhcpServerv4ExclusionRange
+            Mock -CommandName Remove-DhcpServerv4ExclusionRange
 
             It 'Should call "Add-DhcpServerv4ExclusionRange" when "Ensure" = "Present" and exclusion does not exist' {
-                Mock Get-DhcpServerv4ExclusionRange { return $null }
+                Mock -CommandName Get-DhcpServerv4ExclusionRange { return $null }
 
                 Set-TargetResource @testParams -Ensure 'Present'
                 Assert-MockCalled -CommandName Add-DhcpServerv4ExclusionRange
             }
 
             It 'Should call "Remove-DhcpServerv4ExclusionRange" when "Ensure" = "Absent" and exclusion does exist' {
-                Mock Get-DhcpServerv4ExclusionRange -MockWith $getFakeDhcpExclusionRange
+                Mock -CommandName Get-DhcpServerv4ExclusionRange -MockWith $getFakeDhcpExclusionRange
 
                 Set-TargetResource @testParams -Ensure 'Absent'
                 Assert-MockCalled -CommandName Remove-DhcpServerv4ExclusionRange
